e0a17519eab311a3d3917fcf9d513d826b9bffca
champiww <a24sergiobc@iessanclemente.net>
2025-03-05 14:23:16 +0100

    models done

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Degree, Student
 
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    readonly_fields = ("slug", )
+    prepopulated_fields = {"slug": ("name","surname",)}
+    list_filter = ("name", "surname",)
+    list_display = ("name", "surname", "degree.name",)
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Degree)
--- mainApp/models.py
+++ mainApp/models.py
@@ -1,7 +1,28 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 class Degree(models.Model): 
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=1500)
-    numYears = 
+    numYears = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(4)])
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    age = models.IntegerField(validators=[MinValueValidator(1970), MaxValueValidator(2010)])
+    slug = models.SlugField(default='', null=False, db_index=True, unique=True)
+    finished_degree = models.BooleanField(default=False)
+    degree = models.ForeignKey(Degree, on_delete=models.CASCADE, related_name='fkstudents')
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(f"{self.name} {self.surname}")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
