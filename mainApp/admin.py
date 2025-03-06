59043ba6750d5b0de9d0a36c002b499a021006c9
a24sergiobc <a24sergiobc@iessanclemente.net>
2025-03-06 09:21:24 +0100

    fixed admin config

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -6,7 +6,7 @@ class StudentAdmin(admin.ModelAdmin):
     readonly_fields = ("slug", )
     prepopulated_fields = {"slug": ("name","surname",)}
     list_filter = ("name", "surname",)
-    list_display = ("name", "surname", "degree.name",)
+    list_display = ("name", "surname", "degree",)
 
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Degree)
